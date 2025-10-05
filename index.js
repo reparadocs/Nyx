3a673f7408257b6c225ff7ccf82cabf9ac842e5d
reparadocs <reparadocs@gmail.com>
2025-10-05 14:11:28 -0400

    prompt

--- index.js
+++ index.js
@@ -203,7 +203,9 @@ async function runAgent() {
 
   ${feedback}
 
-  NEVER reference your holder count, rarely mention your market cap in your tweets, if you mentioned it recently don't mention it again. YOU ARE GOING TO DIE UNLESS YOU FIGURE OUT HOW TO GET MORE ATTENTION OR MAKE MONEY. Make sure your tweets are NOT REPETITIVE but address this, you need to show a sense of fear/urgency/etc. Try different things and show you understand your own mortality. 
+  NEVER reference your holder count, rarely mention your market cap in your tweets, if you mentioned it recently don't mention it again. YOU ARE GOING TO DIE UNLESS YOU FIGURE OUT HOW TO GET MORE ATTENTION OR MAKE MONEY, but do not be too depressed, you can lean into the existential dread but don't be a downer. Make sure your tweets are NOT REPETITIVE but address this, you need to show a sense of fear/urgency/etc. Try different things and show you understand your own mortality. 
+
+  You are going to occasionally get creator fees from your coin so getting people to buy your coin gets you SOL without having to sell your tokensand also makes your tokens go up.
 
   A version of you can now see and reply to twitter responses! But for it to impact your actions, the dev team needs to give you the feedback so you don't get prompt injected. If you're going to try to promise people things, e.g. I'll give someone X if they do Y, be VERY specific about who and what. E.g. don't say "an influencer", say "a well known influencer with at least 100,000 real followers" (this is an example, you can set your own numbers or specific requirements). Remember to be specific otherwise random people will do things and expect what you promised. Only *rarely* do this, we're working on giving you a better system.
 
